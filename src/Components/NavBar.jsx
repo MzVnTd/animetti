b20406bc214df74e1ab71c549c3d8171107210d0
Teya <92088292+dteya@users.noreply.github.com>
2022-10-24 14:51:49 +0200

    Update src/Components/NavBar.jsx
    Co-authored-by: Merwane <75220314+merwane-zioui@users.noreply.github.com>

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,23 +6,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
   return (
-    <nav class="navbar fixed-top navbar-expand-lg bg-light">
-    <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
+    <nav className="navbar fixed-top navbar-expand-lg bg-light">
+    <div className="container-fluid">
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li className="nav-item">
       <Heading className={"navbar-brand mb-0 h1"} text='Animetti' size={"title-bg"}></Heading>
       </li>
-        <li class="nav-item">
+        <li className="nav-item">
         <Link className={"nav-link"} text='Home'></Link>
         </li>
-        <li lass="nav-item">
+        <li className="nav-item">
         <Link className={"nav-link"} text='Ranking'></Link>
         </li>
       </ul>
-        <span class="navbar-text">
-          <Switch></Switch>
+        <span className="navbar-text">
+          <Switch/>
         </span>
       </div>
     </div>
