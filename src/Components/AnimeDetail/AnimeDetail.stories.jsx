b2e89e8e1efdde1cb0d764cd73c55e2a75334150
Merwane <merwane.zioui@gmail.com>
2022-10-28 13:29:27 +0200

    hotfix(storybook-story): fix storybook for anime detail component

--- src/Components/AnimeDetail/AnimeDetail.stories.jsx
+++ src/Components/AnimeDetail/AnimeDetail.stories.jsx
@@ -12,7 +12,11 @@ export const Default = Template.bind({});
 
 Default.args = {
 	details: {
-		"image": "https://cdn.myanimelist.net/images/anime/1208/94745l.jpg",
+		"images": {
+			"jpg": {
+				"large_image_url": "https://cdn.myanimelist.net/images/anime/1208/94745l.jpg"
+			}
+		},
 		"title": "Fullmetal Alchemist : Brotherhood",
 		"type": "TV",
 		"source": "Manga",
@@ -25,7 +29,9 @@ Default.args = {
 		"members": 3009887,
 		"favorites": 209465,
 		"year": 2009,
-		"trailer_url": "https://www.youtube.com/embed/--IcmZkvL0Q?enablejsapi=1&wmode=opaque&autoplay=1",
+		"trailer": {
+			"embed_url": "https://www.youtube.com/embed/--IcmZkvL0Q?enablejsapi=1&wmode=opaque&autoplay=1"
+		},
 		"synopsis": "After a horrific alchemy experiment goes wrong in the Elric household, brothers Edward and Alphonse are left in a catastrophic new reality. Ignoring the alchemical principle banning human transmutation, the boys attempted to bring their recently deceased mother back to life. Instead, they suffered brutal personal loss: Alphonse's body disintegrated while Edward lost a leg and then sacrificed an arm to keep Alphonse's soul in the physical realm by binding it to a hulking suit of armor. The brothers are rescued by their neighbor Pinako Rockbell and her granddaughter Winry. Known as a bio-mechanical engineering prodigy, Winry creates prosthetic limbs for Edward by utilizing \"automail,\" a tough, versatile metal used in robots and combat armor. After years of training, the Elric brothers set off on a quest to restore their bodies by locating the Philosopher's Stone—a powerful gem that allows an alchemist to defy the traditional laws of Equivalent Exchange. As Edward becomes an infamous alchemist and gains the nickname \"Fullmetal,\" the boys' journey embroils them in a growing conspiracy that threatens the fate of the world. [Written by MAL Rewrite]"
 	}
 };
