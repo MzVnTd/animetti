3a013e422fa0d4936a3699f8bcda6307cc048e47
Merwane <merwane.zioui@gmail.com>
2022-10-28 12:28:16 +0200

    feat(more-element): add more button to load more animes

--- src/Components/Pages/Ranking.jsx
+++ src/Components/Pages/Ranking.jsx
@@ -6,30 +6,28 @@ import Radio from '../Radio/Radio';
 import LateralCard from '../LateralCard/LateralCard';
 import { useEffect, useState } from 'react';
 import Footer from '../Footer/Footer';
+import Button from '../Button/Button';
 
 export default function Ranking() {
-
-	/*    */
-
 	const [animes, setAnimes] = useState([]);
+	const [page, setPage] = useState(1);
+	const [filter, setFilter] = useState('');
 
 	useEffect(() => {
 		fetchAnimes();
-	}, [])
+	}, [page, filter])
 
-	const fetchAnimes = async (selectedFilter) => {
-		console.log(this)
-		const response = await fetch(`https://api.jikan.moe/v4/top/anime?filter=${selectedFilter}&limit=15`);
+	const fetchAnimes = async () => {
+		const response = await fetch(`https://api.jikan.moe/v4/top/anime?filter=${filter}&limit=15&page=${page}`);
 		const animeRecs = await response.json();
-		setAnimes(animeRecs.data);
+		setAnimes(page === 1 ? animeRecs.data : current => [...current , ...animeRecs.data]);
 	}
-
 
 	return (<div>
 		<Navbar />
 		<div className="container mt-5 ml-5 d-flex justify-content-between">
 			<Title text="RANKING" className={'text-uppercase text-left'}></Title>
-			<Radio onChange={(event) => fetchAnimes(event.target.value)}></Radio>
+			<Radio onChange={(event) => {setFilter(event.target.value); setPage(1)}}></Radio>
 		</div>
 		<div className='container align-items-center mt-5'>
 			<div className="container align-items-center"> {
@@ -41,6 +39,7 @@ export default function Ranking() {
 					}
 				)
 			}
+			<Button text="Load more" onClick={() => setPage(page + 1)} />
 			</div>
 		</div>
 		<div>
